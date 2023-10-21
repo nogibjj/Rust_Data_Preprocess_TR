72914ce1feeb5530dc242fdc8f554ede1a5159e7
GitHub Action <action@github.com>
2023-10-21 16:20:20 +0000

    Add metric log

--- test_main.py
+++ test_main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 import psutil
 
+
 def test_get_median():
     # Record the start time
     start_time = time.time()
@@ -10,7 +11,7 @@ def test_get_median():
     example_csv = "Electric_Vehicle_Population_Data.csv"
     df = pd.read_csv(example_csv)
     print(get_median(df["Electric Range"]))
-    
+
     assert get_median(df["Electric Range"]) == 19.0
     end_time = time.time()
     # Calculate the elapsed time
@@ -22,6 +23,7 @@ def test_get_median():
     print(f"Elapsed time: {elapsed_time:.4f} seconds")
     print(f"CPU Usage: {cpu_percent}%")
     print(f"Memory Usage: {memory_info.percent}%")
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     test_get_median()
